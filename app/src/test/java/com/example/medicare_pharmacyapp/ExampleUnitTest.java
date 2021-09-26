a08cbd82edc42077ef9289eb75706ee5ebe534d0
SamudithaD97 <samudithawijesundara@gmail.com>
2021-09-26 15:42:15 +0530

    First commit

---
+++ app/src/test/java/com/example/medicare_pharmacyapp/ExampleUnitTest.java
@@ -0,0 +1,33 @@
+package com.example.medicare_pharmacyapp;
+
+import com.example.medicare_pharmacyapp.Model.Cart;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+
+   /* private My_Cart my_cart;
+
+    @Before
+    public void setUp(){
+
+        my_cart = new My_Cart();
+    }
+    @Test
+    public void oneTypeProductTPrice_isCorrect() {
+
+
+       int oneTypeProductTPrice = my_cart.oneProductPrice(300,3);
+        assertEquals(900, oneTypeProductTPrice, 0.001);
+    }
+*/
+
+}
